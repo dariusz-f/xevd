bc9c244a656899d3054b9b4547ba777021a41893
Dariusz Frankiewicz <5229380+dariusz-f@users.noreply.github.com>
2024-10-08 09:12:19 +0200

    Update xevd_app.c

--- app/xevd_app.c
+++ app/xevd_app.c
@@ -28,22 +28,20 @@
    POSSIBILITY OF SUCH DAMAGE.
 */
 
-
+#include "xevd_app_args.h"
 #include "xevd.h"
 #include "xevd_app_util.h"
-#include "xevd_app_args.h"
+
 
 #define MAX_BS_BUF                 16*1024*1024 /* byte */
 
-static void print_usage(void)
-{
-    int i;
+static void print_usage(void){
+    int  i;
     char str[1024];
 
     logv0("< Usage >\n");
 
-    for(i=0; i<XEVD_NUM_ARG_OPTION; i++)
-    {
+    for(i=0; i<XEVD_NUM_ARG_OPTION; i++) {
         if(xevd_args_get_help(options, i, str) < 0) return;
         logv0("%s\n", str);
     }
@@ -55,14 +53,12 @@ static int read_bitstream(FILE * fp, int * pos, unsigned char * bs_buf)
     unsigned char nalu_len_buf[4], b = 0;
     XEVD_INFO info;
 
-    bs_size = 0;
+    bs_size  =  0;
     read_size = 0;
 
-    if(!fseek(fp, *pos, SEEK_SET))
-    {
+    if(!fseek(fp, *pos, SEEK_SET)){
         /* read size first */
-        if(XEVD_NAL_UNIT_LENGTH_BYTE == fread(nalu_len_buf, 1, XEVD_NAL_UNIT_LENGTH_BYTE, fp))
-        {
+        if(XEVD_NAL_UNIT_LENGTH_BYTE == fread(nalu_len_buf, 1, XEVD_NAL_UNIT_LENGTH_BYTE, fp)) {
             ret = xevd_info(nalu_len_buf, XEVD_NAL_UNIT_LENGTH_BYTE, 1, &info);
             if (XEVD_FAILED(ret)) {
                 logv0("Cannot get bitstream information\n");
